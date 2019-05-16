6239588d15d59ee744a623273458ecf72f3c02ca
christopher <Christopher Harrison>
2019-05-16 12:42:22 -0400

    validateUser to trad function, setup post and user routers

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const moment = require("moment");
 
+const PostsRouter = require("./posts/postsRouter.js");
+const UserRouter = require("./users/userRouter.js");
+
 const server = express();
 server.use(express.json());
-// server.use("/api/posts", PostsRouter)
-server.use((req, res, next) => {
-  console.log(`${req.method} - ${req.url} - ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
-  next();
-});
+server.use("/api/posts", PostsRouter)
+server.user("/api/users", UserRouter)
+server.use(logger);
+
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+function logger(req, res, next) {
+  console.log(
+    `${req.method} - ${req.url} - ${moment().format("MMMM Do YYYY, h:mm:ss a")}`
+  );
+  next();
+}
+
+function validateUser(req, res, next) {}
 module.exports = server;
